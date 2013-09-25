fe497b7894afc050d7c6721c23d1ab08e733e3fa
Med0paW <medopaw@gmail.com>
2013-09-25 15:52:08 +0800

    Move GetFilesytem() into Filesytem

--- dom/base/Navigator.h
+++ dom/base/Navigator.h
@@ -101,10 +101,6 @@ namespace time {
 class TimeManager;
 } // namespace time
 
-namespace filesystem {
-class Filesystem;
-} // namespace filesystem
-
 namespace system {
 #ifdef MOZ_AUDIO_CHANNEL_MANAGER
 class AudioChannelManager;
@@ -214,7 +210,7 @@ public:
   void GetDeviceStorages(const nsAString& aType,
                          nsTArray<nsRefPtr<nsDOMDeviceStorage> >& aStores,
                          ErrorResult& aRv);
-  already_AddRefed<Promise> GetMozFilesystem(const FilesystemParameters& parameters,
+  already_AddRefed<Promise> GetFilesystem(const FilesystemParameters& parameters,
                                              ErrorResult& aRv);
   DesktopNotificationCenter* GetMozNotification(ErrorResult& aRv);
   bool MozIsLocallyAvailable(const nsAString& aURI, bool aWhenOffline,
@@ -352,7 +348,6 @@ private:
   nsRefPtr<nsDOMCameraManager> mCameraManager;
   nsCOMPtr<nsIDOMNavigatorSystemMessages> mMessagesManager;
   nsTArray<nsRefPtr<nsDOMDeviceStorage> > mDeviceStorageStores;
-  nsRefPtr<filesystem::Filesystem> mFilesystem;
   nsRefPtr<time::TimeManager> mTimeManager;
   nsCOMPtr<nsPIDOMWindow> mWindow;
 };
